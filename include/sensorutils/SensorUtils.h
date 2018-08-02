2b0e404b289b20a1d81f9704f0fd6770ffff7338
Makayla Shepherd <mshepherd@usgs.gov>
2018-08-02 09:58:20 -0700

    Adding tests and Tyler's SensorUtils code

---
+++ include/sensorutils/SensorUtils.h
@@ -0,0 +1,11 @@
+#ifndef SensorUtils_h
+#define SensorUtils_h
+#include <vector>
+#include <cmath>
+#include <cfloat>
+#include <climits>
+using namespace std;
+
+double PhaseAngle(vector<double> & instPosition, vector<double> & sunPosition);
+
+#endif
---
+++ src/SensorUtils.cpp
@@ -0,0 +1,10 @@
+#include "SensorUtils.h"
+#include <vector>
+#include <cmath>
+#include <cfloat>
+#include <climits>
+
+
+double PhaseAngle(vector<double>& instrumentPosition,vector<double>& sunPosition) {
+    return 0.0;
+}
