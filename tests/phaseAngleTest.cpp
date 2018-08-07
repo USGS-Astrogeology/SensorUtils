aacde2674d4a489f8e17b89f6a51e1e45a9011eb
--global <--global>
2018-08-07 07:19:24 -0700

    added another test for 100% code coverage.

--- tests/phaseAngleTest.cpp
+++ tests/phaseAngleTest.cpp
@@ -8,12 +8,19 @@ TEST(SensorUtils, PhaseAngle) {
    vector<double> sunPosition1{1, 0, 0};
    vector<double> surfaceIntersection1{0, 0, 0};
    EXPECT_EQ(M_PI, PhaseAngle(instrumentPosition1, sunPosition1, surfaceIntersection1));
-   cout<<"From your test, we got this: "<<PhaseAngle(instrumentPosition1, sunPosition1, surfaceIntersection1)<<std::endl;
+   
    
    vector<double> instrumentPosition2{0, 1, 0};
    vector<double> sunPosition2{0, 1, 0};
    vector<double> surfaceIntersection2{0, 0, 0};
    EXPECT_EQ(0, PhaseAngle(instrumentPosition2, sunPosition2, surfaceIntersection2));
+
+   vector<double> instrumentPosition3{1, 1, 0};
+   vector<double> sunPosition3{-1, 1, 0};
+   vector<double> surfaceIntersection3{0, 0, 0};
+   EXPECT_EQ(M_PI/2.0, PhaseAngle(instrumentPosition3, sunPosition3, surfaceIntersection3));
+   
+   
 }
 
 int main(int argc, char **argv) {
