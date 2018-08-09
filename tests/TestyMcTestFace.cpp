52a5424d851adbdd4715f0b674c3cbe35e103555
Ian Humphrey <ihumphrey@usgs.gov>
2018-08-09 16:36:20 -0700

    Not changing TestyMcTestFace

--- tests/TestyMcTestFace.cpp
+++ tests/TestyMcTestFace.cpp
@@ -2,17 +2,6 @@
 
 #include <gtest/gtest.h>
 
-TEST(SensorUtils, LineResolution) {
-  double focalLength = 500; // mm
-  double lineSumming = 1.0; // no binning
-  double pixelPitch = 0.1; // mm
-  vector<double> instrumentPosition{110.0, 0.0, 0.0}; // km
-  vector<double> surfaceIntersection{100.0, 0.0, 0.0}; // km
-  double resolution = LineResolution(focalLength, lineSumming, pixelPitch, instrumentPosition, surfaceIntersection);
-  std::cout << "resolution: " << resolution << std::endl;
-  EXPECT_EQ(0, resolution);
-}
-
 TEST(SensorUtils, PhaseAngle) {
    vector<double> instrumentPosition{0, 0, 0};
    vector<double> sunPosition{0, 0, 0};
