cb82a103a4e49932661c5fd22bbf445592fa279f
Ian Humphrey <ihumphrey@usgs.gov>
2018-08-13 13:57:37 -0700

    Add SensorCoreTesting

---
+++ tests/SensorCoreTesting.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "CartesianPoint.h"
+#include "ImagePoint.h"
+#include "Sensor.h"
+
+TEST(declination, placeHodor) {
+  Sensor sensor("test", "test");
+  EXPECT_DOUBLE_EQ(0.0, sensor.declination(CartesianVector()));
+}
+
+TEST(emissionAngle, placeHodor) {
+  Sensor sensor("test", "test");
+  EXPECT_DOUBLE_EQ(0.0, sensor.emissionAngle(CartesianPoint()));
+  EXPECT_DOUBLE_EQ(0.0, sensor.emissionAngle(ImagePoint()));
+}
+
+TEST(phaseAngle, placeHodor) {
+  Sensor sensor("test", "test");
+  EXPECT_DOUBLE_EQ(0.0, sensor.phaseAngle(CartesianPoint()));
+  EXPECT_DOUBLE_EQ(0.0, sensor.phaseAngle(ImagePoint()));
+}
+
+TEST(rightAscension, placeHodor) {
+  Sensor sensor("test", "test");
+  EXPECT_DOUBLE_EQ(0.0, sensor.declination(CartesianVector()));
+}
