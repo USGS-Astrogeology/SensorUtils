7ae57dda5048661acc1237011e49e1013a0f9905
Ian Humphrey <ihumphrey@usgs.gov>
2018-08-13 17:13:05 -0700

    Refactor rightAscension() declination()

--- src/sensorcore/Sensor.cpp
+++ src/sensorcore/Sensor.cpp
@@ -5,14 +5,14 @@
 
 #include "sensorcore.h"
 #include "SensorMath.h"
-#include "SensorUtils.h"
 
 Sensor::Sensor(const std::string &metaData, const std::string &sensorName) {
-
 }
 
 double Sensor::declination(const CartesianVector &vector) {
   std::vector<double> cartesianVector{vector.x, vector.y, vector.z};
+  std::vector<double> radiusRaDec = rect2lat(cartesianVector);
+  return radiusRaDec[1];
   return computeRADec(cartesianVector)[1];
 }
 
@@ -34,5 +34,10 @@ double Sensor::phaseAngle(const ImagePoint &imagePoint) {
 
 double Sensor::rightAscension(const CartesianVector &vector) {
   std::vector<double> cartesianVector{vector.x, vector.y, vector.z};
-  return computeRADec(cartesianVector)[0];
+  std::vector<double> radiusRaDec = rect2lat(cartesianVector);
+  if (radiusRaDec[2] < 0.0) {
+    radiusRaDec[2] = 2 * M_PI;
+  }
+  return radiusRaDec[2];
+  // return computeRADec(cartesianVector)[0];
 }
