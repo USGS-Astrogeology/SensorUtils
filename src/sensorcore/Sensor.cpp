d8ffc63f8a953f315dcc468f5f0bb14ea74787ff
Ian Humphrey <ihumphrey@usgs.gov>
2018-08-14 13:02:37 -0700

    Update Sensor with cmath M_PI

--- src/sensorcore/Sensor.cpp
+++ src/sensorcore/Sensor.cpp
@@ -1,5 +1,6 @@
 #include "Sensor.h"
 
+#include <cmath>
 #include <string>
 #include <vector>
 
@@ -21,20 +22,23 @@ double Sensor::declination(const CartesianVector &vector) {
   std::vector<double> cartesianVector{vector.x, vector.y, vector.z};
   std::vector<double> radiusRaDec = rect2lat(cartesianVector);
   return radiusRaDec[1];
-  // return computeRADec(cartesianVector)[1];
 }
+
 
 double Sensor::emissionAngle(const CartesianPoint &groundPoint) {
   return 0.0;
 }
 
+
 double Sensor::emissionAngle(const ImagePoint &imagePoint) {
   return 0.0;
 }
 
+
 double Sensor::phaseAngle(const CartesianPoint &groundPoint) {
   return 0.0;
 }
+
 
 double Sensor::phaseAngle(const ImagePoint &imagePoint) {
   return 0.0;
@@ -52,8 +56,7 @@ double Sensor::rightAscension(const CartesianVector &vector) {
   std::vector<double> cartesianVector{vector.x, vector.y, vector.z};
   std::vector<double> radiusRaDec = rect2lat(cartesianVector);
   if (radiusRaDec[2] < 0.0) {
-    radiusRaDec[2] += 2 * 3.1415926; // pi()
+    radiusRaDec[2] += 2 * M_PI;
   }
   return radiusRaDec[2];
-  // return computeRADec(cartesianVector)[0];
 }
